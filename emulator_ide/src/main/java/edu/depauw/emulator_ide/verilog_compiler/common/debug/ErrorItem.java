ced255eca3d0b8a94d7d88f7c0bf22d40b3989bb
h20man13 <jacobbauer_2021@depauw.edu>
2021-01-22 21:00:07 -0500

    Added class for logging Errors

---
+++ emulator_ide/src/main/java/edu/depauw/emulator_ide/verilog_compiler/common/debug/ErrorItem.java
@@ -0,0 +1,26 @@
+package edu.depauw.emulator_ide.verilog_compiler.common.debug;
+
+import edu.depauw.emulator_ide.verilog_compiler.common.token.Position;
+    
+import java.lang.String;
+import java.lang.StringBuilder;
+    
+public class ErrorItem implements InfoItem{
+    
+    private final Position position;
+    private final String message;
+
+    public ErrorItem(String message, Position position){
+	this.message = message;
+	this.position = position;
+    }
+	
+    public String toString(){
+	StringBuilder result = new StringBuilder("\u001b[31;1mError\u001b[37m:\u001b[0m ");
+	result.append(message);
+	result.append(" at [");
+	result.append(position.toString());
+	result.append(']');
+	return result.toString();
+    }
+}
